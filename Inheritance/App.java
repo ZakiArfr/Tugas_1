d6c98093bf14d38b26416704c193ce27bab6c2fb
ZakiArfr <zakiarif2006@gmail.com>
2026-02-10 14:17:55 +0700

    Update App.java

--- Inheritance/App.java
+++ Inheritance/App.java
@@ -1,15 +1,16 @@
 public class App {
     public static void main(String[] args) {
 
-        bujurSangkar bujursangkar = new bujurSangkar("Bujur Sangkar", 34);
+        bujurSangkar bujursangkar = new bujurSangkar("Bujur Sangkar", 4);
 
         bujursangkar.infoBidang();
         System.out.println("Luas Bujur Sangkar: " + bujursangkar.luas());
         System.out.println();
-        lingkaran bundaran = new lingkaran("Lingkaran", 11);
+        lingkaran bundaran = new lingkaran("Lingkaran", 8);
 
         bundaran.infoBidang();
         System.out.println("Luas Lingkaran: " + bundaran.luas());
     }
+
 
 }
